28e99ddf7b7b22e0e88de7d21cee917e0e0e66da
FedorovaElizaveta <147994204+FedorovaElizaveta@users.noreply.github.com>
2024-09-12 15:12:43 +0300

    Add auth logic and models

---
+++ src/db/models/session.js
@@ -0,0 +1,29 @@
+import { model, Schema } from "mongoose";
+
+const sessionSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    accessToken: {
+      type: String,
+      required: true,
+    },
+    refreshToken: {
+      type: String,
+      required: true,
+    },
+    accessTokenValidUntil: {
+      type: Date,
+      required: true,
+    },
+    refreshTokenValidUntil: {
+      type: Date,
+      required: true,
+    },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+export const Session = model("Session", sessionSchema);
