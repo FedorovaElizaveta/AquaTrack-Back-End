271960a2ff456273913ead29815a2b45bc059870
FedorovaElizaveta <147994204+FedorovaElizaveta@users.noreply.github.com>
2024-09-15 11:50:31 +0300

    Added auth middleware to patch user router

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -14,6 +14,7 @@ import {
   registerLoginUserSchema,
   patchUserSchema,
 } from '../validation/auth.js';
+import { auth } from '../middlewares/auth.js';
 
 const router = express.Router();
 const jsonParser = express.json();
@@ -38,6 +39,7 @@ router.post('/refresh', ctrlWrapper(refreshTokensController));
 
 router.patch(
   '/profile',
+  auth,
   jsonParser,
   validateBody(patchUserSchema),
   ctrlWrapper(patchUserController),
