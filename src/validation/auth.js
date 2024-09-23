468626864b015bfb08b5741f7ca02a659b11bca2
FedorovaElizaveta <147994204+FedorovaElizaveta@users.noreply.github.com>
2024-09-23 19:56:30 +0300

    Update swagger

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -2,7 +2,7 @@ import Joi from 'joi';
 
 export const registerLoginUserSchema = Joi.object({
   email: Joi.string().email().required(),
-  password: Joi.string().required(),
+  password: Joi.string().min(6).max(30).required(),
 });
 
 export const patchUserSchema = Joi.object({
