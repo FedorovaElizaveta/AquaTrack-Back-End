f859cb68b69b121788b282d8b9e23e1b98bd6e8e
FedorovaElizaveta <147994204+FedorovaElizaveta@users.noreply.github.com>
2024-09-23 22:38:27 +0300

    Fix errorHandler

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,17 +1,18 @@
 import { HttpError } from 'http-errors';
 
-export const errorHandler = (err, req, res) => {
+export const errorHandler = (err, req, res, next) => {
   if (err instanceof HttpError) {
     res.status(err.status).json({
       status: err.status,
-      message: err.message,
+      message: err.name,
       data: err,
     });
-  } else {
-    res.status(500).json({
-      status: 500,
-      message: 'Something went wrong',
-      data: process.env.NODE_ENV === 'development' ? err.stack : err.message,
-    });
+    return;
   }
+
+  res.status(500).json({
+    status: 500,
+    message: 'Something went wrong',
+    data: err.message,
+  });
 };
